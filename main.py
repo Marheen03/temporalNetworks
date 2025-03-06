a8c9627a29cdef438287380b268d73212369272b
Marin Rabađija <150812305+Marheen03@users.noreply.github.com>
2025-03-06 14:21:21 +0100

    Added community detection with Girvan-Newman

--- main.py
+++ main.py
@@ -9,9 +9,22 @@ isolatedNodes = 0
 snapshots = 0
 maxCommunitySize = 0
 
+communityDetection = "x"
+
 for i, graph_path in enumerate(snapshot_graphs.values()):
     G = nx.read_gml(graph_path)
-    communities = nx.community.louvain_communities(G, weight="count")
+    if communityDetection == "girvan_newman":
+        communitiesIterator = nx.community.girvan_newman(G, utils.most_central_edge)
+        communities = list(sorted(c) for c in next(communitiesIterator))
+
+        isolatedCommunities = 0
+        for community in communities:
+            if len(community) == 1:
+                isolatedCommunities += 1
+        isolatedNodes += isolatedCommunities
+    else:
+        communities = nx.community.louvain_communities(G, weight="count")
+        isolatedCommunities = 0
 
     # finding the largest community
     largestCommunity = len(max(communities, key=len))
@@ -19,15 +32,20 @@ for i, graph_path in enumerate(snapshot_graphs.values()):
 
     numberOfCommunities += len(communities)
     isolatedNodes += len(utils.find_isolated_nodes(communities))
-    
-    snapshots += 1
-    #print("{}. snapshot:".format(i+1), isolatedNodes)
-
+    #print("{}. snapshot:".format(i+1), len(utils.find_isolated_nodes(communities)) + isolatedCommunities)
+ 
     communityOfNode = utils.get_community_of_node(communities)
     #print("{}. snapshot:".format(i+1), communityOfNode)
+    snapshots += 1
 
+if communityDetection == "girvan_newman":
+    print("GIRVAN-NEWMANOV ALGORITAM")
+else:
+    print("LOUVAINOV ALGORITAM")
 
 print("Duljina najveće zajednice:", maxCommunitySize)
 print("Ukupan broj zajednica:", numberOfCommunities)
 print("Prosječan broj zajednica:", numberOfCommunities / snapshots)
+
+print("Ukupan broj izoliranih mušica:", isolatedNodes)
 print("Prosječan broj izoliranih mušica:", isolatedNodes / snapshots)
--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
-import os, sys, re
+import os, sys, re, networkx as nx
+
+# removes edge with the highest centrality based on weight
+def most_central_edge(G):
+    centrality = nx.edge_betweenness_centrality(G, weight="count")
+
+    return max(centrality, key=centrality.get)
+
 
 def natural_sort(l):
     def convert(text):
@@ -38,35 +45,6 @@ def load_files_from_folder(path, file_format=".csv", n_sort=False):
     return files_dict
 
 
-def get_community_of_node(communities, numOfFlies=12):
-    """
-    Returns community ID in which given node is part of.
-
-    Parameters:
-    communities (list): List of communities represented as sets.
-    numOfFlies (int): Total number of observed flies.
-
-    Returns:
-    dict: flies as keys and community ID as values (-1 if node isn't part of community).
-    """
-
-    allFlies = []
-    for i in range(numOfFlies):
-        fly = 'fly' + str(i+1)
-        allFlies.append(fly)
-
-    communityOfNode = {}
-    for fly in allFlies:
-        for i, community in enumerate(communities):
-            # check if fly is within certain community
-            if fly in community:
-                communityOfNode[fly] = i+1
-            else:
-                communityOfNode[fly] = -1
-    
-    return communityOfNode
-
-
 def find_isolated_nodes(communities, numOfFlies=12):
     """
     Returns isolated nodes (flies) from given snapshot.
@@ -91,3 +69,32 @@ def find_isolated_nodes(communities, numOfFlies=12):
     
     isolatedNodes = allFlies.difference(currentCommunity)
     return isolatedNodes
+
+
+def get_community_of_node(communities, numOfFlies=12):
+    """
+    Returns community ID in which given node is part of.
+
+    Parameters:
+    communities (list): List of communities represented as sets.
+    numOfFlies (int): Total number of observed flies.
+
+    Returns:
+    dict: flies as keys and community ID as values (-1 if node isn't part of community).
+    """
+
+    allFlies = []
+    for i in range(numOfFlies):
+        fly = 'fly' + str(i+1)
+        allFlies.append(fly)
+
+    communityOfNode = {}
+    for fly in allFlies:
+        for i, community in enumerate(communities):
+            # check if fly is within certain community
+            if fly in community:
+                communityOfNode[fly] = i+1
+                break
+            communityOfNode[fly] = -1
+    
+    return communityOfNode
